58a3a536b889ffbfdb73a038b59cb9c50e428c05
Beom Sik <ryuha_____@naver.com>
2021-04-22 23:10:41 +0900

    커밋

--- src/test/java/CustomCalculatorTest.java
+++ src/test/java/CustomCalculatorTest.java
@@ -1,4 +1,41 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 public class CustomCalculatorTest {
-  
+
+    private CustomCalculator customCalculator;
+
+    //더하기 테스트
+
+    @Test
+    public void testAdd() {
+        customCalculator = new CustomCalculator();
+        int result = customCalculator.add(10, 10);
+        /* 테스트 코드 입력*/
+        assertTrue(result == 20);
+        assertFalse(result == 19);
+    }
+
+    @Test
+    public void testSubtact(){
+        customCalculator = new CustomCalculator();
+        int m_result = customCalculator.subtract(30,10);
+
+        /* 테스트 코드 입력*/
+        assertTrue(m_result == 20);
+    }
+
+    @Test
+    public void testMultiply(){
+        customCalculator = new CustomCalculator();
+        assertTrue(customCalculator.multiply(2,3) == 6);
+    }
+
+    @Test
+    public void testDivide(){
+        customCalculator = new CustomCalculator();
+        assertTrue(customCalculator.divide(25,5) == 5);
+    }
+
 }
--- src/test/java/UpperCaseCounterTest.java
+++ src/test/java/UpperCaseCounterTest.java
@@ -17,13 +17,17 @@ public class UpperCaseCounterTest {
 
         int numberOfUpperCaseCharactersInString = upperCaseCounter.getNumberOfUpperCaseCharactersInString(str);
         assertTrue(numberOfUpperCaseCharactersInString == 0);
+        assertThat(numberOfUpperCaseCharactersInString, is(0));
+        System.out.println("result :: " + numberOfUpperCaseCharactersInString);
     }
 
     //반겂울 전달했을 때 0을 리턴하는지에 대한 테스트 작성
     @Test
     public void getNumberOfUpperCaseCharactersInString_return_0_for_empty_input() {
         String str = "";
-
+        int numberOfUpperCaseCharactersInString = upperCaseCounter.getNumberOfUpperCaseCharactersInString(str);
+        assertTrue(numberOfUpperCaseCharactersInString == 0);
+        System.out.println("result :: " + numberOfUpperCaseCharactersInString);
 //        int numberOfUpperCaseCharactersInString = upperCaseCounter.getNumberOfUpperCaseCharactersInString(str);
     }
 
@@ -35,8 +39,12 @@ public class UpperCaseCounterTest {
         int numberOfUpperCaseCharactersInString = upperCaseCounter.getNumberOfUpperCaseCharactersInString(str);
 
         //assertTrue로 맞는 테스트 코드 작성
+        assertTrue(numberOfUpperCaseCharactersInString == 10);
         //assertFalse로 틀리는 값을 넣어 테스트 작성
+        assertFalse(numberOfUpperCaseCharactersInString == 9 );
         //assertThat 단정문을 사용해서 True인 테스트 코드 작성
+        assertThat(numberOfUpperCaseCharactersInString, is(10));
+
     }
 
     //대소문자가 섞여 있을 때 정확히 카운팅 되는 지에 대한 테스트 코드 작성
@@ -45,20 +53,24 @@ public class UpperCaseCounterTest {
         String str = "ABCdefGHI";
         int result = upperCaseCounter.getNumberOfUpperCaseCharactersInString(str);
         //assertTrue로 맞는 테스트 코드 작성
+        assertTrue(result == 6);
         //assertFalse로 틀리는 값을 넣어 테스트 작성
+        assertFalse(result != 6);
         //assertThat 단정문을 사용해서 True인 테스트 코드 작성
+        assertThat(result, is(6));
         System.out.println("result :: " + result);
     }
 
     //잘못된 값을 참조했을 때 IndexOutOfBoundsException Exception이 발생하는지 테스트 코드 작성
-    @Test
+    @Test(expected = IndexOutOfBoundsException.class)
     public void shouldThrowExceptionWhenGetZeroIndex() {
         new ArrayList<Object>().get(0);
     }
 
+
     //해당 메소드가 제한된 시간내에 실행되는지에 대한 테스트 코드 작성 : timeout 사용
     //두번째로 해당 메소드는 테스트 하지 않도록 어노테이션 추가 적용 해봅니다. Ignore
-    @Test
+    @Test(timeout = 5000)
     public void testShouldRunInLimitedTime() throws InterruptedException {
         Thread.sleep(4000);
         System.out.println("제한된 시간 내에 수행되면 테스트 Passed!");
